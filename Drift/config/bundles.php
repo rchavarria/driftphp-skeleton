569c35a3589889df8a2446b3c7a4d9b82484f753
Rubén Chavarría <txingo@gmail.com>
2020-07-22 09:35:44 +0200

    Activar el command bus bundle para poder inyectar el command/query bundle en los controladores

--- Drift/config/bundles.php
+++ Drift/config/bundles.php
@@ -14,5 +14,6 @@
 declare(strict_types=1);
 
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+  Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => [ 'all' => true ],
+  Drift\CommandBus\CommandBusBundle::class => [ 'all' => true ]
 ];
