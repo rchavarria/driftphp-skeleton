d8c7ae379bec56051e7b45dc9008534e27c0748b
Rubén Chavarría <txingo@gmail.com>
2020-07-28 19:17:29 +0200

    Enable DBAL bundle

--- Drift/config/bundles.php
+++ Drift/config/bundles.php
@@ -15,5 +15,6 @@ declare(strict_types=1);
 
 return [
   Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => [ 'all' => true ],
+  Drift\DBAL\DBALBundle::class => [ 'all' => true ],
   Drift\CommandBus\CommandBusBundle::class => [ 'all' => true ]
 ];
