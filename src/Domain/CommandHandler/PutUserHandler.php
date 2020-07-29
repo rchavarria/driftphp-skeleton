484938f6369fd8491c960906b6f90c61997e85de
Rubén Chavarría <txingo@gmail.com>
2020-07-29 19:10:17 +0200

    Send our UserWasSaved event to the event bus

--- src/Domain/CommandHandler/PutUserHandler.php
+++ src/Domain/CommandHandler/PutUserHandler.php
@@ -3,16 +3,21 @@
 namespace Domain\CommandHandler;
 
 use Domain\Command\PutUser;
+use Domain\Event\UserWasCreated;
 use Domain\Model\User\UserRepository;
+use Drift\EventBus\Bus\EventBus;
 use React\Promise\PromiseInterface;
 
 class PutUserHandler {
 
   /** @var UserRepository */
   private $userRepository;
+  /** @var EventBus */
+  private $eventBus;
 
-  public function __construct(UserRepository $userRepository) {
+  public function __construct(UserRepository $userRepository, EventBus $eventBus) {
     $this->userRepository = $userRepository;
+    $this->eventBus = $eventBus;
   }
 
   /**
@@ -23,7 +28,12 @@ class PutUserHandler {
   public function handle(PutUser $putUser): PromiseInterface {
     return $this
       ->userRepository
-      ->save($putUser->getUser());
+      ->save($putUser->getUser())
+      ->then(function () use ($putUser) {
+        return $this
+          ->eventBus
+          ->dispatch(new UserWasCreated($putUser->getUser()));
+      });
   }
 
 }
