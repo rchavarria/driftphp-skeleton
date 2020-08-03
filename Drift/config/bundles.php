117fa367881e54c5179057b7faafd6500e435a06
Rubén Chavarría <txingo@gmail.com>
2020-08-03 20:47:09 +0200

    Enable WebsocketBundle

--- Drift/config/bundles.php
+++ Drift/config/bundles.php
@@ -17,5 +17,6 @@ return [
   Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => [ 'all' => true ],
   Drift\DBAL\DBALBundle::class => [ 'all' => true ],
   Drift\CommandBus\CommandBusBundle::class => [ 'all' => true ],
-  Drift\EventBus\EventBusBundle::class => [ 'all' => true ]
+  Drift\EventBus\EventBusBundle::class => [ 'all' => true ],
+  Drift\Websocket\WebsocketBundle::class => [ 'all' => true ]
 ];
